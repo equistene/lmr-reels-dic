f80b368261dbcdb2446e52501e624a5d70bb5299
Cristian Berrios <equistene@gmail.com>
2025-12-15 17:30:34 -0300

    chore: create api and components dirs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
+const baseConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+})(baseConfig);
